4a2ca344e9e99f81929afe7d3a88f520eb4e7b82
Jason Etcovitch <jasonetco@github.com>
2018-11-06 12:03:31 -0500

    Make them tests ts

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,4 +2,8 @@ describe('actions-toolkit', () => {
   it('has a test', () => {
     expect(1 + 1).toBe(2)
   })
+
+  it('?', () => {
+    expect(process.env.HOME).toBe('?')
+  })
 })
--- tests/setup.ts
+++ tests/setup.ts
@@ -1,4 +1,4 @@
-const path = require('path')
+import path from 'path';
 
 Object.assign(process.env, {
   HOME: '?',
