1c544970e0f2772c69dd0132e9385cc529384773
Jason Etcovitch <jasonetco@github.com>
2019-01-27 14:25:30 -0500

    Improve types

--- src/github.ts
+++ src/github.ts
@@ -1,8 +1,9 @@
+import { GraphQlQueryResponse, Variables } from '@octokit/graphql'
 import Octokit from '@octokit/rest'
 import { withDefaults } from './graphql'
 
 export class GitHub extends Octokit {
-  public graphql: (query: string, variables?: any) => any
+  public graphql: (query: string, variables?: Variables) => Promise<GraphQlQueryResponse>
 
   constructor (token: string) {
     super({ auth: `token ${token}` })
