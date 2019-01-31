25ca89dd20fa0455483a2f773b45ec6d22fb3331
Jason Etcovitch <jasonetco@github.com>
2019-01-31 14:58:36 -0500

    Implementt

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,19 @@
 import fs from 'fs'
 
-export default class Store extends Map {
+export default class Store extends Map<string, any> {
+  constructor (path?: string) {
+    if (path && fs.existsSync(path)) {
+      const contents = fs.readFileSync(path, 'utf8')
+      const json = JSON.parse(contents)
+      super(json)
+    } else {
+      super()
+    }
+  }
+
   public async write (path: string) {
     return new Promise((resolve, reject) => {
-      const entries = this.entries()
-      const contents = JSON.stringify(entries)
+      const contents = JSON.stringify(Array.from(this))
       fs.writeFile(path, contents, (err) => {
         if (err) return reject(err)
         return resolve()
