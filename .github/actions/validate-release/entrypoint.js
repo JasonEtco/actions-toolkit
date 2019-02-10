463f3fa59616d1ccd93621a8ff1da97fd256c16e
Jason Etcovitch <jasonetco@github.com>
2019-02-10 12:01:15 -0500

    Use exit methods

--- .github/actions/validate-release/entrypoint.js
+++ .github/actions/validate-release/entrypoint.js
@@ -5,42 +5,34 @@ const tools = new Toolkit()
 const pkg = tools.getPackageJSON()
 const { release } = tools.context.payload
 
-// Little helper function to log an error then exit
-function fail (message) {
-  console.error(message)
-  process.exit(1)
-}
-
 if (release.draft) {
-  console.log('This release is a draft! Aborting.')
-  // Exiting with a 78 makes it a "Neutral" result
-  process.exit(78)
+  tools.exit.neutral('This release is a draft! Aborting.')
 }
 
 const tag = semver.valid(release.tag_name)
 if (!tag) {
-  fail(`The tag ${release.tag_name} is not a valid tag.`)
+  tools.exit.failure(`The tag ${release.tag_name} is not a valid tag.`)
 }
 
 if (pkg.version !== tag) {
-  fail(`Tag ${tag} and version in the package.json ${pkg.version} are not the same.`)
+  tools.exit.failure(`Tag ${tag} and version in the package.json ${pkg.version} are not the same.`)
 }
 
 if (release.prerelease) {
   const prerelease_tag = semver.prerelease(release.tag_name)
 
   if (prerelease_tag === null) {
-    fail(`The release is a prerelease, but the version tag is not.`)
+    tools.exit.failure(`The release is a prerelease, but the version tag is not.`)
   }
 
   const VALID_TAGS = ['beta', 'next']
   const [tag_name] = prerelease_tag
 
   if (!VALID_TAGS.includes(tag_name)) {
-    fail(`Publish tag ${tag_name} is not a valid tag - it must be one of ${VALID_TAGS.join(', ')}`)
+    tools.exit.failure(`Publish tag ${tag_name} is not a valid tag - it must be one of ${VALID_TAGS.join(', ')}`)
   }
 
   fs.writeFileSync(path.join(tools.workspace, 'release-workflow-tag'), tag_name)
 }
 
-console.log(`Release of version ${tag} is all set!`)
+tools.exit.success(`Release of version ${tag} is all set!`)
