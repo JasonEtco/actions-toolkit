47781c9fcbfb036b4c88b5b219d2d1d0de42b93f
Jason Etcovitch <jasonetco@github.com>
2019-02-24 13:40:28 -0500

    Add get-body to get the body off the payload

---
+++ src/get-body.ts
@@ -0,0 +1,13 @@
+import { WebhookPayloadWithRepository } from './context'
+
+/**
+ * Get the body of the relevant comment, review, issue or pull request
+ * @param payload - Webhook payload
+ */
+export function getBody (payload: WebhookPayloadWithRepository): string | undefined {
+  if (payload.comment) return payload.comment.body
+  if (payload.review) return payload.review.body
+  // If neither of those comments are present, check the body
+  if (payload.issue) return payload.issue.body
+  if (payload.pull_request) return payload.pull_request.body
+}
