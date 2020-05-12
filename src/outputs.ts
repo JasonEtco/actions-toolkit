a06700e3d723ab82d92ab0a7f854dd2bbb949cfc
abouroubi <abouroubi@gmail.com>
2020-05-12 20:43:50 +0200

    Update src/outputs.ts
    fix: typo
    
    Co-authored-by: Jason Etcovitch <jasonetco@github.com>

--- src/outputs.ts
+++ src/outputs.ts
@@ -5,7 +5,7 @@ export interface OutputType { [key: string]: string | undefined }
 export function createOutputProxy <O extends OutputType = OutputType>() {
   return new Proxy<O>({} as O, {
     set (_, name: string, value: string): boolean {
-      // When we attempt to get `outputs.___`, instead
+      // When we attempt to set `outputs.___`, instead
       // we call `core.setOutput`.
       core.setOutput(name, value)
       return true;
