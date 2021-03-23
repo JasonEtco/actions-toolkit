5d904e0043492a536cb9447ae383da25ada6aa6d
Federico Grandi <fgrandi30@gmail.com>
2021-03-23 23:37:38 +0100

    test(logger): add logger tests
    The change to target=es6 in the tsconfig is due to the fact that tests fail because CustomSignale is a ES6 class.

--- src/logger.ts
+++ src/logger.ts
@@ -1,15 +1,19 @@
 import { Signale, DefaultMethods } from 'signale'
 
+export const defaultCommands: [DefaultMethods, string][] = [
+  ['error', '::error::'],
+  ['fatal', '::error::'],
+  ['warn', '::warning::'],
+  ['debug', '::debug::']
+]
+
 export class CustomSignale extends Signale {
   constructor() {
     super({
       config: { underlineLabel: false }
     })
 
-    this.addPrefix('error', '::error::')
-    this.addPrefix('fatal', '::error::')
-    this.addPrefix('warn', '::warning::')
-    this.addPrefix('debug', '::debug::')
+    defaultCommands.forEach(([key, prefix]) => this.addPrefix(key, prefix))
   }
 
   public startGroup(title: string) {
@@ -27,6 +31,8 @@ export class CustomSignale extends Signale {
     const method = this[key]
 
     this[key] = (...args: any[]) => {
+      let message, options
+
       if (
         args.length == 1 &&
         typeof args[0] == 'object' &&
@@ -35,16 +41,17 @@ export class CustomSignale extends Signale {
       ) {
         // The method has been called using only one config argument
         // logger.error({ message: 'abc', ... })
-        return args[0].noIssue
-          ? method(...args)
-          : method({ prefix, ...args[0] })
+        options = args[0]
       } else {
         // The method has been called using message + options
         // logger.error('abc', { ... })
-        return args[1].noIssue
-          ? method(...args)
-          : method(args[0], { prefix, ...(args[1] ?? {}) })
+        message = args[0]
+        options = args[1]
       }
+
+      if (!options) options = {}
+      if (message) options = { ...options, message }
+      return options.noIssue ? method(...args) : method({ prefix, ...options })
     }
   }
 }
---
+++ tests/logger.test.ts
@@ -0,0 +1,89 @@
+import { CustomSignale, defaultCommands } from '../src/logger'
+
+describe('defaultCommands', () => {
+  it('should be an array of 2-strings arrays', () => {
+    expect(
+      defaultCommands.every(
+        (e) =>
+          e instanceof Array &&
+          e.length == 2 &&
+          e.every((s) => !!s && typeof s == 'string')
+      )
+    ).toBeTruthy()
+  })
+})
+
+describe('CustomSignale', () => {
+  const logger = new CustomSignale(),
+    spy = jest.spyOn(process.stdout, 'write')
+
+  afterEach(() => {
+    spy.mockReset()
+  })
+
+  describe('#startGroup', () => {
+    it('should exist in the custom class', () => {
+      expect(typeof logger.startGroup).toBe('function')
+    })
+
+    it('should issue a "group" command with the correct title', () => {
+      logger.startGroup('asd123-')
+      expect(spy).toHaveBeenCalledTimes(1)
+      expect(spy).toHaveBeenLastCalledWith('::group::asd123-')
+    })
+  })
+
+  describe('#endGroup', () => {
+    it('should exist in the custom class', () => {
+      expect(typeof logger.endGroup).toBe('function')
+    })
+
+    it('should issue a "endgroup" command', () => {
+      logger.endGroup()
+      expect(spy).toHaveBeenCalledTimes(1)
+      expect(spy).toHaveBeenLastCalledWith('::endgroup::')
+    })
+  })
+
+  describe.each(defaultCommands)('%s -> %s', (key, prefix) => {
+    it('should work with one config object', () => {
+      logger[key]({ message: 'asd' })
+      expect(spy).toHaveBeenCalledTimes(1)
+      expect(spy).toHaveBeenLastCalledWith(
+        expect.stringMatching(new RegExp(`^${prefix}`, 'g'))
+      )
+    })
+
+    it('should work with message + config object', () => {
+      logger[key]('asd', {})
+      expect(spy).toHaveBeenCalledTimes(1)
+      expect(spy).toHaveBeenLastCalledWith(
+        expect.stringMatching(new RegExp(`^${prefix}`, 'g'))
+      )
+    })
+
+    it('should work with just the message', () => {
+      logger[key]('asd')
+      expect(spy).toHaveBeenCalledTimes(1)
+      expect(spy).toHaveBeenLastCalledWith(
+        expect.stringMatching(new RegExp(`^${prefix}`, 'g'))
+      )
+    })
+
+    it('should work with config and no-issue option', () => {
+      logger[key]({ message: 'asd', noIssue: true })
+      expect(spy).toHaveBeenCalledTimes(1)
+      expect(spy).toHaveBeenLastCalledWith(
+        expect.not.stringMatching(new RegExp(`^${prefix}`, 'g'))
+      )
+    })
+
+    it('should work with message + no-issue option', () => {
+      logger[key]('asd', { noIssue: true })
+      expect(spy).toHaveBeenCalledTimes(1)
+      expect(spy).toHaveBeenLastCalledWith(
+        expect.not.stringMatching(new RegExp(`^${prefix}`, 'g'))
+      )
+    })
+  })
+})
